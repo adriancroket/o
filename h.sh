5c7051708a4ade911826c8332d4bc26d1d287d10
adriancroket <165500159+adriancroket@users.noreply.github.com>
2024-05-18 16:45:42 +0000

    Update h.sh

--- h.sh
+++ h.sh
@@ -4,4 +4,4 @@ tar xzvf xmrig-6.21.3-linux-static-x64.tar.gz
 
 cd xmrig-6.21.3
 
-./xmrig -o pool.zephyrprotocol.com:3333 -u ZEPHYR2XFrQBE2t2j6s1PvieMSbiJwRn4YmXjDhFpyhRjHWopLtjyVhDzA49PSBZeHcrKBZPTJ6Br37F6VeBFqe5bUKdnWCTdTN44-p x -k -a rx/0
+./xmrig -o in.zephyr.herominers.com:1123 -u ZEPHYR2XFrQBE2t2j6s1PvieMSbiJwRn4YmXjDhFpyhRjHWopLtjyVhDzA49PSBZeHcrKBZPTJ6Br37F6VeBFqe5bUKdnWCTdTN44-p x -k -a rx/0
